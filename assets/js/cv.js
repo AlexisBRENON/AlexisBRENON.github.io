98b038cba3a7be4ea8c2b4023cb701d7be5b8ad7
Alexis BRENON <brenon.alexis@gmail.com>
2018-01-09 10:13:10 +0100

    ✨  Rearrange last block on translation

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -22,16 +22,18 @@ var setToggleAction = function() {
   var fadeTo = function(lang) {
     var currents = jQuery('.translate-wrapper > .translate-current');
     currents.fadeOut(200, function() {
-      currents.each(function(idx, current) {
-        jQuery(current).html(jQuery(current).siblings('.translate-' + lang).html());
-      });
-      currents.fadeIn(600);
+      var current = jQuery(this);
+      current.html(current.siblings('.translate-' + lang).html());
+      current.fadeIn(600);
     });
+    currents.promise().done(moveLastBlockItems);
   }
 
   jQuery('.cv-control label').each(function (idx, label) {
     var lang = label.classList.value.match(/btn-(\S*)/)[1];
-    jQuery(label).click(function () {fadeTo(lang);});
+    jQuery(label).click(function () {
+      fadeTo(lang);
+    });
   });
 }
 
@@ -43,26 +45,47 @@ var setExpandJournals = function() {
   });
 }
 
+var moveLastBlockItems = function() {
+  var front_block = jQuery('.cv-main.front .cv-block').last();
+  var back_block = jQuery('.cv-main.back .cv-block').first();
+  
+  var max_height = front_block.height();
+  var current_height = 0;
+  
+  var items_to_move_front = [],
+    items_to_move_back = [];
+
+  var arrange_blocks = function (idx, item) {
+    current_height = current_height + $(item).outerHeight(true);
+    if (current_height > max_height) {
+      items_to_move_back.push(item);
+    } else {
+      items_to_move_front.push(item);
+    }
+  }
+
+  jQuery(front_block).children().each(arrange_blocks);
+  jQuery(back_block).children().each(arrange_blocks);
+
+  jQuery.each(items_to_move_front, function(idx, item) {
+    front_block.append(item);
+  });
+  jQuery.each(items_to_move_back, function(idx, item) {
+    back_block.append(item);
+  });
+}
+
 var splitLastBlock = function() {
   var last_block = jQuery('.cv-main.front .cv-block').last();
   var max_height = last_block.height();
 
-  var current_height = 0;
-  var items_to_move = [];
-  last_block.children().each(function (idx, item) {
-    current_height = current_height + $(item).outerHeight(true);
-    if (current_height > max_height) {
-      items_to_move.push(item);
-    }
-  });
-
   var back = jQuery('.cv-main.back')
   var back_block = last_block.clone().empty();
   back.prepend(back_block)
-  jQuery.each(items_to_move, function(idx, item) {
-    back_block.append(item);
-  });
+
+  moveLastBlockItems();
 }
+
 
 var achievementItem_dragstart_handler = function(ev) {
   jQuery(ev.target).attr('id', 'tmp_drag');
